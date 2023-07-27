6420aa9a61b31008d70992959fc057fb06c53e5f
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-07-27 14:40:53 +0200

    add beautiful css for the form

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ let myLibrary = [];
 
 function Book(title, author, numOfPages, isRead) {
   this.title = title;
-  this.author = title;
+  this.author = author;
   this.numOfPages = numOfPages;
   this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, numOfPages, isRead) {
+  let book = new Book(title, author, numOfPages, isRead);
+  myLibrary.push(book);
 }
