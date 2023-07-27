2174eefdb6a2f77b99dfef5f87a4b8f65e8d0af8
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-07-27 15:30:35 +0200

    add basic toggle for the form

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+let booksDiv = document.querySelector(".books");
+let showBtn = document.querySelector(".show");
+let wrapper = document.querySelector(".wrapper");
+let form = document.querySelector(".form");
+
 let myLibrary = [];
+let book1 = new Book("Sapiens", "Yuval Hoah Farari", 200, false);
+let book2 = new Book("Ishmael", "Daniel Quinn", 282, true);
+myLibrary.push(book1, book2);
 
 function Book(title, author, numOfPages, isRead) {
   this.title = title;
@@ -11,3 +19,26 @@ function addBookToLibrary(title, author, numOfPages, isRead) {
   let book = new Book(title, author, numOfPages, isRead);
   myLibrary.push(book);
 }
+
+for (const book of myLibrary) {
+  const div = document.createElement("div");
+  const paraTitle = document.createElement("p");
+  const paraAuthor = document.createElement("p");
+  const paraPages = document.createElement("p");
+  const paraDidRead = document.createElement("p");
+  div.appendChild(paraTitle);
+  div.appendChild(paraAuthor);
+  div.appendChild(paraPages);
+  div.appendChild(paraDidRead);
+
+  paraTitle.textContent = book.title;
+  paraAuthor.textContent = book.author;
+  paraPages.textContent = book.numOfPages;
+  paraDidRead.textContent = book.isRead;
+  booksDiv.appendChild(div);
+}
+
+showBtn.addEventListener("click", (e) => {
+  wrapper.classList.toggle("active");
+  form.classList.toggle("active");
+});
