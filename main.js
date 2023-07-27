4066b334d061d67ccb19f3be7f693982e45264c5
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-07-27 11:19:40 +0200

    tidy up js

--- main.js
+++ main.js
@@ -11,29 +11,27 @@ function addBookToLibrary() {
   // do stuff here
 }
 
-let isMousedown = false;
 let body = document.querySelector("body");
 let wrapper = document.querySelector(".wrapper");
+let isMousedown = false;
 let timesPerSecond = 60; // how many times to fire the event per second
 let wait = false;
 
-function callback(e) {
+body.addEventListener("mousedown", (e) => {
+  isMousedown = true;
+  animateDiv(e);
+});
+
+body.addEventListener("mousemove", (e) => throttleEvent(e, animateDiv));
+
+body.addEventListener("mouseup", (e) => {
+  isMousedown = false;
+  resetDiv();
+});
+
+function animateDiv(e) {
   if (isMousedown) {
-    let center = { x: wrapper.offsetLeft + 250, y: wrapper.offsetTop + 250 };
-    let xVector;
-    let yVector;
-    if (e.x > center.x) {
-      xVector = Math.abs(e.x - center.x);
-    } else {
-      xVector = -Math.abs(e.x - center.x);
-    }
-
-    if (e.y > center.y) {
-      yVector = Math.abs(e.y - center.y);
-    } else {
-      yVector = -Math.abs(e.y - center.y);
-    }
-
+    const [xVector, yVector] = calculateVector(e);
     wrapper.style.transform = `translate(${xVector}px, ${yVector}px) scale(0.2)`;
     wrapper.style.background = "#2c0505";
     wrapper.style.borderRadius = "50%";
@@ -43,17 +41,34 @@ function callback(e) {
   }
 }
 
-body.addEventListener("mouseup", (e) => {
-  isMousedown = false;
+function resetDiv() {
   wrapper.style.background = "#181818";
   wrapper.style.transform = "scale(1)";
   wrapper.style.borderRadius = "20px";
   wrapper.style.boxShadow = "0px 10px 15px 5px #413131";
   wrapper.style.border = "none";
   wrapper.style.fontSize = "1rem";
-});
+}
 
-body.addEventListener("mousemove", (e) => {
+function calculateVector(e) {
+  let center = { x: wrapper.offsetLeft + 250, y: wrapper.offsetTop + 250 };
+  let xVector;
+  let yVector;
+  if (e.x > center.x) {
+    xVector = Math.abs(e.x - center.x);
+  } else {
+    xVector = -Math.abs(e.x - center.x);
+  }
+
+  if (e.y > center.y) {
+    yVector = Math.abs(e.y - center.y);
+  } else {
+    yVector = -Math.abs(e.y - center.y);
+  }
+  return [xVector, yVector];
+}
+
+function throttleEvent(e, callback) {
   // don't handle events when one has just occurred
   if (!wait) {
     // fire the event
@@ -65,9 +80,4 @@ body.addEventListener("mousemove", (e) => {
       wait = false;
     }, 1000 / timesPerSecond);
   }
-});
-
-body.addEventListener("mousedown", (e) => {
-  isMousedown = true;
-  callback(e);
-});
+}
