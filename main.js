5d6c7c2b26fcc9077acedd76488e64bd06aadaf5
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-07-27 17:14:31 +0200

    add book addition functionality
    On form submit, fetch input values and addBookToLibrary(), then reset
    the 'booksDiv', then populate 'booksDiv' with added book added to the
    end of array

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ let booksDiv = document.querySelector(".books");
 let showBtn = document.querySelector(".show");
 let wrapper = document.querySelector(".wrapper");
 let form = document.querySelector(".form");
+let addBtn = document.querySelector(".btn.add");
 
 let myLibrary = [];
 let book1 = new Book("Sapiens", "Yuval Hoah Farari", 200, false);
-let book2 = new Book("Ishmael", "Daniel Quinn", 282, true);
-let book3 = new Book("Mastery", "Robert Greene", 338, true);
-myLibrary.push(book1, book2, book3);
+myLibrary.push(book1);
+populateBooks();
 
 function Book(title, author, numOfPages, isRead) {
   this.title = title;
@@ -21,27 +21,48 @@ function addBookToLibrary(title, author, numOfPages, isRead) {
   myLibrary.push(book);
 }
 
-for (const book of myLibrary) {
-  const div = document.createElement("div");
-  const paraTitle = document.createElement("p");
-  const paraAuthor = document.createElement("p");
-  const paraPages = document.createElement("p");
-  const paraDidRead = document.createElement("p");
-  div.appendChild(paraTitle);
-  div.appendChild(paraAuthor);
-  div.appendChild(paraPages);
-  div.appendChild(paraDidRead);
+function removeChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.lastChild);
+  }
+}
 
-  div.className = "book";
-  paraTitle.textContent = book.title;
-  paraAuthor.textContent = book.author;
-  paraPages.textContent = book.numOfPages;
-  paraDidRead.textContent = book.isRead;
-  booksDiv.appendChild(div);
+function populateBooks() {
+  for (const book of myLibrary) {
+    const div = document.createElement("div");
+    const paraTitle = document.createElement("p");
+    const paraAuthor = document.createElement("p");
+    const paraPages = document.createElement("p");
+    const paraDidRead = document.createElement("p");
+    div.appendChild(paraTitle);
+    div.appendChild(paraAuthor);
+    div.appendChild(paraPages);
+    div.appendChild(paraDidRead);
+
+    div.className = "book";
+    paraTitle.textContent = book.title;
+    paraAuthor.textContent = book.author;
+    paraPages.textContent = book.numOfPages;
+    paraDidRead.textContent = book.isRead;
+    booksDiv.appendChild(div);
+  }
 }
 
 showBtn.addEventListener("click", (e) => {
   wrapper.classList.toggle("active");
   form.classList.toggle("active");
   showBtn.textContent = `${showBtn.textContent === "Show form" ? "Close form" : "Show form"}`;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let title = e.target.title.value;
+  let author = e.target.author.value;
+  let numPages = e.target.numOfPages.value;
+  let isRead = e.target.isRead.value;
+
+  addBookToLibrary(title, author, numPages, isRead);
+  removeChildren(booksDiv);
+  populateBooks();
 });
